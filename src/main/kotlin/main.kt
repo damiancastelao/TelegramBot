42bdfd15d60966ee5488f2fb634c2e5868162748
Damian Nogueiras <damiannogueiras@gmail.com>
2021-04-15 19:15:20 +0200

    markdown

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,6 +3,7 @@ import com.github.kotlintelegrambot.dispatch
 import com.github.kotlintelegrambot.dispatcher.command
 import com.github.kotlintelegrambot.dispatcher.text
 import com.github.kotlintelegrambot.entities.ChatId
+import com.github.kotlintelegrambot.entities.ParseMode
 import com.github.kotlintelegrambot.network.fold
 
 fun main() {
@@ -30,6 +31,28 @@ fun main() {
                         {
                             // do something with the error
                         }
+                )
+            }
+            command("arrancar1") {
+                val markdownV2Text = """
+                    *bold \*text*
+                    _italic \*text_
+                    __underline__
+                    ~strikethrough~
+                    *bold _italic bold ~italic bold strikethrough~ __underline italic bold___ bold*
+                    [inline URL](http://www.example.com/)
+                    [inline mention of a user](tg://user?id=123456789)
+                    `inline fixed-width code`
+                    ```kotlin
+                    fun main() {
+                        println("Hello Kotlin!")
+                    }
+                    ```
+                """.trimIndent()
+                bot.sendMessage(
+                    chatId = ChatId.fromId(message.chat.id),
+                    text = markdownV2Text,
+                    parseMode = ParseMode.MARKDOWN_V2
                 )
             }
         }
