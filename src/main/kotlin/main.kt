7487f4f0f34fa2f71a1a39e1da974e68233ef8f3
Damian Nogueiras <damiannogueiras@gmail.com>
2021-04-15 18:50:41 +0200

    Eco y comando

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,39 @@
 import com.github.kotlintelegrambot.bot
+import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.dispatcher.text
+import com.github.kotlintelegrambot.entities.ChatId
+import com.github.kotlintelegrambot.network.fold
 
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    println("Hello Bot!")
+
+    val bot = bot {
+
+        token = "1701880343:AAHdH2zYMwCUEYCrr3eaneDHlKTL5ucvtyg"
+
+        dispatch {
+
+            text {
+                print("Chat ID: ")
+                println(message.chat.id)
+                bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = text)
+            }
+            command("para1") {
+
+                val result = bot.sendMessage(chatId = ChatId.fromId(update.message!!.chat.id), text = "Para world!")
+
+                result.fold(
+                        {
+                            // do something here with the response
+                        },
+                        {
+                            // do something with the error
+                        }
+                )
+            }
+        }
+    }
+
+    bot.startPolling()
 }
